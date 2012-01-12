2814c0dc7c9882f9b5d6b27145faff57016fab8b
Helen Ma <ethlite@mac.com>
2012-01-11 22:47:36 -0800

    Initial Commit

---
+++ Reader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Reader
+//
+//  Created by Helen Ma on 1/11/12.
+//  Copyright (c) 2012 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
